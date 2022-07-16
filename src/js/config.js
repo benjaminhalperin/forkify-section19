be5505574bff32334393359d9dd0fa29af4403eb
benjaminhalperin <32729118+benjaminhalperin@users.noreply.github.com>
2022-07-16 10:01:18 +0300

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const TIMEOUT_SEC = 5;
+export const RES_PER_PAGE = 10;
+export const KEY = '05bf17c0-a3e2-4724-8eb6-0e0e6a5a1213';
+export const MODAL_CLOSE_SEC = 2.5;
